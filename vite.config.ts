82dbf763595f5c2d0a5b5faada0227ffa181f5d3
童圣滔 <tongshengtao@bosssoft.com.cn>
2025-05-23 16:54:46 +0800

    release:适配新版nest的第一版前端

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/gateway': {
-        //target: 'http://118.178.197.208:9981',
-        target: 'http://localhost:3001',
+        target: 'http://118.178.197.208:9981',
+        //target: 'http://localhost:3001',
         //target: 'http://118.178.197.208:9980',
         changeOrigin: true, // 是否改变源，后端收到的请求头中host会是目标地址
         rewrite: (path) => path.replace(/^\/gateway/, ''), // 将gateway去掉，否则后端收到的请求路径是/gateway/user/login
